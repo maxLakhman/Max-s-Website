458c4af5b4c1d010db4f3692a385d8989d09daeb
maxLakhman <msinghla@buffalo.edu>
2023-07-31 18:29:58 -0400

    added a CSS file and the smiley image that now scales

--- hello.js
+++ hello.js
@@ -1,1 +1,5 @@
-console.log("hello");
+// Identifying our image
+const smiley = document.querySelector("#smiley");
+// Adding reaction when image is clicked
+smiley.addEventListener("click", () => {    alert(`Smiley says "Welcome!"`);
+});
